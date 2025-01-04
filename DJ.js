75ad4c938092773716824c605f97d391d9f3ee14
zhien99 <1301039667@qq.com>
2025-01-05 01:48:13 +0800

    Update DJ.js

--- DJ.js
+++ DJ.js
@@ -1,11 +1,11 @@
 /******************************
  
-脚本名称:Do音乐盒
+脚本名称:DJ6.20.115
 
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/zhien99/X/refs/heads/main/DJ.js
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body DJYYH.js
 
 
 [mitm]
@@ -13,6 +13,7 @@ hostname = apphy2.djyule.com
 %¥
 *******************************/
 var body=$response.body;
-body = body.replace(/><HYVIP>0</HYVIP><VIPdate></VIPdate>/g,'><HYVIP>1</HYVIP><VIPdate>2099/09/09</VIPdate>');
+body = body.replace(/<HYVIP>.*?</HYVIP>/g,'<HYVIP>1</HYVIP>');
+body = body.replace(/<VIPdate>.*?</VIPdate>/g,'<VIPdate>2099/09/09</VIPdate>');
 body = body.replace(/VIPgrade>0</VIPgrade>/g,'VIPgrade>1</VIPgrade>');
 $done(body);
