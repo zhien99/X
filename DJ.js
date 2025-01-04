4476d1c5f35a2ce1384c2a4d07ccd3a9ff896c05
zhien99 <1301039667@qq.com>
2025-01-05 01:56:48 +0800

    Update DJ.js

--- DJ.js
+++ DJ.js
@@ -1,19 +1,21 @@
 /******************************
  
-脚本名称:DJ6.20.115
+脚本名称: DJYule VIP 解锁
 
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/zhien99/X/refs/heads/main/DJ.js
-
+^https:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp$ url script-response-body DJYule.js
 
 [mitm]
 hostname = apphy2.djyule.com
 %¥
 *******************************/
-var body=$response.body;
-body = body.replace(/<HYVIP>.*?</HYVIP>/g,'<HYVIP>1</HYVIP>');
-body = body.replace(/<VIPdate>.*?</VIPdate>/g,'<VIPdate>2099/09/09</VIPdate>');
-body = body.replace(/VIPgrade>0</VIPgrade>/g,'VIPgrade>1</VIPgrade>');
+var body = $response.body;
+
+// 修改 VIP 状态
+body = body.replace(/<HYVIP>.*?<\/HYVIP>/g, '<HYVIP>1</HYVIP>');
+body = body.replace(/<VIPdate>.*?<\/VIPdate>/g, '<VIPdate>2099/09/09</VIPdate>');
+body = body.replace(/VIPgrade>0<\/VIPgrade>/g, 'VIPgrade>1</VIPgrade>');
+
 $done(body);
