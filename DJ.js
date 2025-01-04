8101e36f8c2cd3c7efd5ab0e86cfe964fe131bed
zhien99 <1301039667@qq.com>
2025-01-04 22:15:32 +0800

    更新 DJ.js

--- DJ.js
+++ DJ.js
@@ -1,122 +1,18 @@
 /******************************
  
-脚本名称:测试DJ
+脚本名称:Do音乐盒
 
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp|https?:\/\/apphy2\.djyule\.com.*?.*? url script-response-body https://raw.githubusercontent.com/zhien99/X/refs/heads/main/DJ.js
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body YYH.js
 
 
 [mitm]
-hostname = apphy2.djyule.com,apphy2.djyule.com
+hostname = apphy2.djyule.com
 %¥
 *******************************/
-let body=$response.body;
-body = body.replace(/<HYVIP>\d</HYVIP>/g,'<HYVIP>1</HYVIP>');
-body = body.replace(/VIPdate>.*?</VIPdate>/g,'VIPdate>1</VIPdate>');
-body = body.replace(/<VIPdate>.*?</VIPdate>/g,'<VIPdate>2999/09/09</VIPdate>');
-body = body.replace(/<VIPgrade>\d</VIPgrade>/g,'<VIPgrade>1</VIPgrade>');
-body = body.replace(/<showPlayTingSQ>\d</showPlayTingSQ>/g,'<showPlayTingSQ>1</showPlayTingSQ>');
-body = body.replace(/<JinBi>.*?</JinBi>/g,'<JinBi>666666</JinBi>');
-body = body.replace(/<downBi>\d+</downBi>/g,'<downBi>0</downBi>');
-body = body.replace(/<duJiaZZ>\d</duJiaZZ>/g,'<duJiaZZ>0</duJiaZZ>');
-$done(body);{
-  "rules" : [
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<HYVIP>1<\/HYVIP>",
-      "matchValue" : "<HYVIP>\\d<\/HYVIP>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "VIPdate>1<\/VIPdate>",
-      "matchValue" : "VIPdate>.*?<\/VIPdate>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<VIPdate>2999\/09\/09<\/VIPdate>",
-      "matchValue" : "<VIPdate>.*?<\/VIPdate>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<VIPgrade>1<\/VIPgrade>",
-      "matchValue" : "<VIPgrade>\\d<\/VIPgrade>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<showPlayTingSQ>1<\/showPlayTingSQ>",
-      "matchValue" : "<showPlayTingSQ>\\d<\/showPlayTingSQ>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<JinBi>666666<\/JinBi>",
-      "matchValue" : "<JinBi>.*?<\/JinBi>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<downBi>0<\/downBi>",
-      "matchValue" : "<downBi>\\d+<\/downBi>",
-      "destiontion" : "response",
-      "isRegex" : true
-    },
-    {
-      "action" : "body",
-      "matchField" : "",
-      "field" : "",
-      "value" : "<duJiaZZ>0<\/duJiaZZ>",
-      "matchValue" : "<duJiaZZ>\\d<\/duJiaZZ>",
-      "destiontion" : "response",
-      "isRegex" : true
-    }
-  ],
-  "enabled" : false,
-  "name" : "测试DJ",
-  "description" : "",
-  "locations" : [
-    {
-      "method" : "GET",
-      "scheme" : "https",
-      "enabled" : true,
-      "port" : 443,
-      "query" : "",
-      "host" : "apphy2.djyule.com",
-      "path" : "\/userLogin_2022md5.asp"
-    },
-    {
-      "method" : "GET",
-      "scheme" : "https",
-      "enabled" : true,
-      "port" : 0,
-      "query" : "",
-      "host" : "apphy2.djyule.com",
-      "path" : ""
-    }
-  ]
-}
+var body=$response.body;
+body = body.replace(/><HYVIP>0</HYVIP><VIPdate></VIPdate>/g,'><HYVIP>1</HYVIP><VIPdate>2099/09/09</VIPdate>');
+body = body.replace(/VIPgrade>0</VIPgrade>/g,'VIPgrade>1</VIPgrade>');
+$done(body);
