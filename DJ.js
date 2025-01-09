4243d9cfe56069a40d5472fed78149bdffb57a79
zhien99 <1301039667@qq.com>
2025-01-10 07:39:52 +0800

    更新 DJ.js

--- DJ.js
+++ DJ.js
@@ -12,13 +12,8 @@
 hostname = apphy2.djyule.com
 %¥
 *******************************/
-
-// 混淆后的脚本
-(function() {
-    var _0x4f1d=["\x72\x65\x70\x6C\x61\x63\x65","\x3C\x48\x59\x56\x49\x50\x3E\x2E\x2A\x3F\x3C\x2F\x48\x59\x56\x49\x50\x3E\x2Fg","\x3C\x56\x49\x50\x64\x61\x74\x65\x3E\x2E\x2A\x3F\x3C\x2F\x56\x49\x50\x64\x61\x74\x65\x3E\x2Fg","\x3C\x56\x49\x50\x67\x72\x61\x64\x65\x3E\x30\x3C\x2F\x56\x49\x50\x67\x72\x61\x64\x65\x3E","\x31\x3C\x2F\x56\x49\x50\x67\x72\x61\x64\x65\x3E","\x32\x30\x39\x39\x2F\x30\x39\x2F\x30\x39","\x2F\x67","\x62\x6F\x64\x79"];
-    var _0x5e22 = $response[_0x4f1d[0]];
-    _0x5e22 = _0x5e22[_0x4f1d[2]](new RegExp(_0x4f1d[1]), _0x4f1d[3]);
-    _0x5e22 = _0x5e22[_0x4f1d[2]](new RegExp(_0x4f1d[4]), _0x4f1d[5]);
-    _0x5e22 = _0x5e22[_0x4f1d[2]](new RegExp(_0x4f1d[6]), _0x4f1d[7]);
-    $done(_0x5e22);
-})();
+var body = $response.body;
+body = body.replace(/<HYVIP>.*?<\/HYVIP>/g, '<HYVIP>1</HYVIP>');
+body = body.replace(/<VIPdate>.*?<\/VIPdate>/g, '<VIPdate>2099/09/09</VIPdate>');
+body = body.replace(/VIPgrade>0<\/VIPgrade>/g, 'VIPgrade>1</VIPgrade>');
+$done(body);
