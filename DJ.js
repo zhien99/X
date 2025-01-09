5a115b4479693cd5c31ce6630aae8dfa96059a0c
zhien99 <1301039667@qq.com>
2025-01-09 09:24:23 +0800

    更新 DJ.js

--- DJ.js
+++ DJ.js
@@ -12,25 +12,12 @@
 hostname = apphy2.djyule.com
 %¥
 *******************************/
-(function(_0x1a27dd, _0x281dd9) {
-    var _0x5c862b = function(_0x3991e6) {
-        while (--_0x3991e6) {
-            _0x1a27dd['push'](_0x1a27dd['shift']());
-        }
-    };
-    _0x5c862b(++_0x281dd9);
-}(_0x1a27, 0x112));
-var _0x5c86 = function(_0x1a27dd, _0x281dd9) {
-    _0x1a27dd = _0x1a27dd - 0x0;
-    var _0x5c862b = _0x1a27[_0x1a27dd];
-    return _0x5c862b;
-};
-var _0x17d7b2 = _0x5c86;
+
 (function() {
-    var _0x1705f0 = _0x17d7b2;
-    var _0x442560 = $response[_0x1705f0('0x0')];
-    _0x442560 = _0x442560[_0x1705f0('0x1')](/<HYVIP>.*?<\/HYVIP>/g, _0x1705f0('0x2'));
-    _0x442560 = _0x442560[_0x1705f0('0x1')](/<VIPdate>.*?<\/VIPdate>/g, _0x1705f0('0x3'));
-    _0x442560 = _0x442560[_0x1705f0('0x1')](/VIPgrade>0<\/VIPgrade>/g, _0x1705f0('0x4'));
-    $done(_0x442560);
+    var _0x2f3b=["\x72\x65\x70\x6C\x61\x63\x65","\x2F\x3C\x48\x59\x56\x49\x50\x3E\x2E\x2A\x3F\x3C\x2F\x48\x59\x56\x49\x50\x3E\x2Fg","\x2F\x3C\x56\x49\x50\x64\x61\x74\x65\x3E\x2E\x2A\x3F\x3C\x2F\x56\x49\x50\x64\x61\x74\x65\x3E\x2Fg","\x3C\x56\x49\x50\x67\x72\x61\x64\x65\x3E\x30\x3C\x2F\x56\x49\x50\x67\x72\x61\x64\x65\x3E","\x31\x3C\x2F\x56\x49\x50\x67\x72\x61\x64\x65\x3E","\x32\x30\x39\x39\x2F\x30\x39\x2F\x30\x39","\x2F\x67","\x62\x6F\x64\x79"];
+    var _0x13b0 = $response[_0x2f3b[0]];
+    _0x13b0 = _0x13b0[_0x2f3b[2]](new RegExp(_0x2f3b[1]), _0x2f3b[3]);
+    _0x13b0 = _0x13b0[_0x2f3b[2]](new RegExp(_0x2f3b[4]), _0x2f3b[5]);
+    _0x13b0 = _0x13b0[_0x2f3b[2]](new RegExp(_0x2f3b[6]), _0x2f3b[7]);
+    $done(_0x13b0);
 })();
