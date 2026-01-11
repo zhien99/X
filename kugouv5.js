92e6449bec22d9e85d840af3bbf568c0dd19c466
zhien99 <1301039667@qq.com>
2026-01-12 00:37:10 +0800

    更新 qingting.js

--- kugouv5.js
+++ kugouv5.js
@@ -1,1 +1,14 @@
-1
+// qingting.js
+let obj = {
+  code: 0,
+  msg: "success",
+  data: {
+    vipValid: true,
+    svipValid: true,
+    vipType: "svip",
+    vipEndTime: "2099-12-31 23:59",
+    svipEndTime: "2099-12-31 23:59"
+  }
+};
+
+$done({ body: JSON.stringify(obj) });
