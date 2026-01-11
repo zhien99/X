b5e3694f7c06ea64c50622ede70fa720fddc812f
zhien99 <1301039667@qq.com>
2026-01-12 00:52:08 +0800

    更新 kugouv5.js

--- kugouv5.js
+++ kugouv5.js
@@ -1,25 +1,21 @@
-/*
+/******************************
+ 
+脚本名称:蜻蜓
 
-
+*******************************
+%¥
 [rewrite_local]
-^https:\/\/api\.beedbank\.com\/proxy\/user\/my\/info$ url script-response-body https://raw.githubusercontent.com/zhien99/X/refs/heads/main/kugouv5.js
+^https?:\/\/api\.beedbank\.com\/proxy\/user\/my\/info.*? url script-response-body https://raw.githubusercontent.com/zhien99/X/refs/heads/main/kugouv5.js
+
 
 [mitm]
 hostname = api.beedbank.com
-
-*/
-
-// qingting.js
-let obj = {
-  code: 0,
-  msg: "success",
-  data: {
-    vipValid: true,
-    svipValid: true,
-    vipType: "svip",
-    vipEndTime: "2099-12-31 23:59",
-    svipEndTime: "2099-12-31 23:59"
-  }
-};
-
-$done({ body: JSON.stringify(obj) });
+%¥
+*******************************/
+let body=$response.body;
+body = body.replace(/vipValid\":\w+/g,'vipValid":true');
+body = body.replace(/svipValid\":\w+/g,'svipValid":true');
+body = body.replace(/vipType\":"\w+"/g,'vipType":"svip"');
+body = body.replace(/vipEndTime\":"\.*?"/g,'vipEndTime":"2099-01-09 18:37"');
+body = body.replace(/svipEndTime\":"\.*?"/g,'svipEndTime":"2099-01-09 17:13"');
+$done(body);
